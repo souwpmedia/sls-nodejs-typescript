4ec1555d18f114502870ab1df6a014a6ac82247e
Charles Chong <ilovesouwp@gmail.com>
2021-06-02 10:25:21 -0500

    add orm code

---
+++ src/migration/1622646030127-Photo.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class Photo1622646030127 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
